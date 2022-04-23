47903d814d2bd05f24e6f3f90ceb6d23521d472a
Gitesh Sharma <gitesh15796@gmail.com>
2022-04-23 22:41:59 +0530

    event emiiter, input, output added

--- poc/src/app/app.component.ts
+++ poc/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'poc';
+  foods: string[] = [];
+
+  constructor() {
+  }
+
+  addFood(foodItem: string) {
+    this.foods.push(foodItem);
+  }
+
+  removeFood(indexFood: number) {
+    this.foods = this.foods.filter((val, index) => index != indexFood)
+  }
 }
--- poc/src/app/child/child.component.ts
+++ poc/src/app/child/child.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-child',
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./child.component.css']
 })
 export class ChildComponent implements OnInit {
+  @Input('foods') foodArr: string[] = [];
+  @Output('deleteEvt') foodDelete = new EventEmitter<number>();
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  deleteItem(index: number) {
+    this.foodDelete.emit(index);
+  }
 }
